a29c4bdadbe703cd2e4126fcf504be20bf19fbfa
vikasroy63 <65035091+vikasroy63@users.noreply.github.com>
2020-05-09 02:06:51 +0530

    Add files via upload

--- admin/Public/CC_support_component.php
+++ admin/Public/CC_support_component.php
@@ -32,11 +32,40 @@
 **/
 
 include '../lib/admin.defines.php';
+include '../lib/admin.module.access.php';
 include '../lib/admin.smarty.php';
+include '../lib/support/classes/support_service.php';
+include '../lib/Form/Class.FormHandler.inc.php';
+include './form_data/FG_var_support_component.inc';
 
-getpost_ifset(array (
-    'error'
-));
+if (!has_rights(ACX_SUPPORT)) {
+    Header("HTTP/1.0 401 Unauthorized");
+    Header("Location: PP_error.php?c=accessdenied");
+    die();
+}
 
-$smarty -> assign("error", $error);
-$smarty -> display('index.tpl');
+$HD_Form->setDBHandler(DbConnect());
+$HD_Form->init();
+
+if ($id != "" || !is_null($id)) {
+    $HD_Form->FG_EDITION_CLAUSE = str_replace("%id", "$id", $HD_Form->FG_EDITION_CLAUSE);
+}
+
+if (!isset ($form_action))
+    $form_action = "list"; //ask-add
+if (!isset ($action))
+    $action = $form_action;
+
+$list = $HD_Form->perform_action($form_action);
+$smarty->display('main.tpl');
+
+// #### HELP SECTION
+echo $CC_help_support_component;
+
+// #### TOP SECTION PAGE
+$HD_Form->create_toppage($form_action);
+
+$HD_Form->create_form($form_action, $list, $id = null);
+
+// #### FOOTER SECTION
+$smarty->display('footer.tpl');
